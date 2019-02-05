455e2668bc892a2c51ca743a2e3b3f9240e557dd
rupinder <Rupinder.Kaur@ssb.no>
2019-02-05 14:31:25 +0100

    Added disabled test and readme about displayname

--- src/test/java/learning/JUnit5/annotations/examples/LifecycleJUnit5Test.java
+++ src/test/java/learning/JUnit5/annotations/examples/LifecycleJUnit5Test.java
@@ -3,6 +3,7 @@ package learning.JUnit5.annotations.examples;
 import org.junit.jupiter.api.*;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @DisplayName("JUnit-5 test cases")
 public class LifecycleJUnit5Test {
@@ -17,11 +18,21 @@ public class LifecycleJUnit5Test {
         System.out.println("@BeforeEach executed");
     }
 
-    @DisplayName("Sample test-1")
+    @DisplayName("Sample test-1 \uD83D\uDE31 ")
     @Test
     void testAssertEquals() {
         System.out.println("======TEST ONE EXECUTED=======");
         assertEquals(6, Stream.of(1, 2, 3)
+                .mapToInt(i -> i)
+                .sum(), () -> "Sum should be equal to 6");
+    }
+
+    @Disabled
+    @DisplayName("Sample test-2")
+    @Test
+    void testAssertNotEquals() {
+        System.out.println("======TEST TWO EXECUTED=======");
+        assertNotEquals(5, Stream.of(1, 2, 3)
                 .mapToInt(i -> i)
                 .sum(), () -> "Sum should be equal to 6");
     }
